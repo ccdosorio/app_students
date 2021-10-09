57ee72a9021bfaddf5a9b48e8620ae85b8accb22
ccdosorio <ccdosorio11@gmail.com>
2021-10-09 13:59:28 -0600

    Inicio del proyecto

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:app_students/screens/screens.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Students App',
+      initialRoute: 'login',
+      routes: {
+        'login': (_) => LoginScreen(),
+        'home': (_) => HomeScreen(),
+      },
+    );
+  }
+}
---
+++ lib/screens/screens.dart
@@ -0,0 +1,2 @@
+export 'package:app_students/screens/home_screen.dart';
+export 'package:app_students/screens/login_screen.dart';
